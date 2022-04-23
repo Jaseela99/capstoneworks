cc960717804a14dccf6af88d21877cd7f220b3af
Jaseela99 <jaseelachittur@gmail.com>
2022-04-23 20:28:09 +0530

    added rating

--- frontend/src/components/user/Rating.js
+++ frontend/src/components/user/Rating.js
@@ -5,16 +5,16 @@ const rating = () => {
       <div>
     <div class="rating rating-lg rating-half">
   <input type="radio" name="rating-10" class="rating-hidden"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-1"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-2"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-1" defaultChecked/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-2"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-1"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-2"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-1"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-2"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-1"/>
-  <input type="radio" name="rating-10" class="bg-green-500 mask mask-star-2 mask-half-2"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-1"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-2"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-1" defaultChecked/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-2"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-1"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-2"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-1"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-2"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-1"/>
+  <input type="radio" name="rating-10" class="bg-yellow-400 mask mask-star-2 mask-half-2"/>
 </div>
 </div>
   )
